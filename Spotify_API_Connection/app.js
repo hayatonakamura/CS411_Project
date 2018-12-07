e9b84dd3ca7d06c48460aa105d236b5dfb121dd8
MaggieZZY <ziyaoz@bu.edu>
2018-12-07 18:33:13 -0500

    update

--- Spotify_API_Connection/app.js
+++ Spotify_API_Connection/app.js
@@ -28,57 +28,73 @@ app.set('view engine', 'pug');
 var clientID = '';
 var clientSecret = '';
 var callbackURL = 'http://localhost:3000/logindone';
-// passport.use(new GoogleStrategy({
-//     clientID:'7083216799-e7v5oqfmrrq1uhmblnknprcvulvrtku8.apps.googleusercontent.com',
-//     ClientSecret: '6_vUFQS2p_d1k_TCpgUVxGq2',
-//     callbackURL:"http://localhost:3000/logindone"
-// },
-// function(accessToken, refreshToken, profile, cb){
-//     User.findOrCreate({
-//         googleId: profile.id}, function(err,user){
-//                 console.log(profile.id)
-//                 return cb(err,user);
+
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+  
+var stateKey = 'spotify_auth_state';
+  
+app.get('/login', function(req, res) {
+  
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+  
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email user-library-modify playlist-read-private playlist-modify-private playlist-modify-public';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: clientID,
+      scope: scope,
+      redirect_uri: callbackURL,
+      state: state
+    }));
+});
+
+
+// passport.use(
+//     new SpotifyStrategy(
+//       {
+//         clientID: '168bf21f216b4bd1af6d571c272ee031',
+//         clientSecret: '2e00b9b143ec47e3bf737ba7492127d0',
+//         callbackURL: 'http://localhost:3000/logindone'
+//       },
+//       function(accessToken, refreshToken, expires_in, profile, done) {
+//         User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
+//           return done(err, user);
 //         });
+//       }
+//     )
+//   );
+
+// app.get('/login', (req,res)=>{
+//     res.render(__dirname + '/view/login.pug');
+// })
+
+// app.get(
+//     '/spotifyauth',
+//     passport.authenticate('spotify', { failureRedirect: '/login' }, {scope: ['user-read-email', 'user-read-private', 'user-library-modify', 'playlist-modify-private', 'playlist-read-private', 'playlist-modify-public'],showDialog: true}),
+//     function(req, res) {
+//     console.log(req.body)
+//       // Successful authentication, redirect home.
+//       res.redirect('/logindone');
 //     }
-
-// ));
-
-passport.use(
-    new SpotifyStrategy(
-      {
-        clientID: '168bf21f216b4bd1af6d571c272ee031',
-        clientSecret: '2e00b9b143ec47e3bf737ba7492127d0',
-        callbackURL: 'http://localhost:3000/logindone'
-      },
-      function(accessToken, refreshToken, expires_in, profile, done) {
-        User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
-          return done(err, user);
-        });
-      }
-    )
-  );
+// );
 
 
 app.get('/',(req,res)=>{  
-    res.render(__dirname + '/view/index.pug');
+  res.render(__dirname + '/view/index.pug');
 
 });
 
-app.get('/login', (req,res)=>{
-    res.render(__dirname + '/view/login.pug');
-})
-
-app.get(
-    '/spotifyauth',
-    passport.authenticate('spotify', { failureRedirect: '/login' }, {scope: ['user-read-email', 'user-read-private', 'user-library-modify'],showDialog: true}),
-    function(req, res) {
-    console.log(req.body)
-      // Successful authentication, redirect home.
-      res.redirect('/logindone');
-    }
-);
-
-
+var access_token;
 app.get('/logindone',(req,res)=>{
     var code = req.query.code || null;
 
@@ -97,7 +113,7 @@ app.get('/logindone',(req,res)=>{
 
 
     request.post(authOptions, function(error, response, body) {
-      var access_token = body.access_token;
+      access_token = body.access_token;
       var options = {
         url: 'https://api.spotify.com/v1/me',
         headers: { 'Authorization': 'Bearer ' + access_token },
@@ -107,14 +123,8 @@ app.get('/logindone',(req,res)=>{
         console.log(body.id);
       });
 
-      // res.redirect('/#' +
-      // querystring.stringify({
-      //   access_token: access_token,
-      // }));
-
     });
-  
-    res.render(__dirname + '/view/loggedin.pug');
+    res.render(__dirname + '/view/loggedin.pug');  
 });
 
 
@@ -123,10 +133,12 @@ app.listen( port,() => {
 console.log('Server is up and running on port numner ' + port);
 });
 
-app.post('/parsed',(req,res)=>{
-    cloudvision(req,res);
-});
 
+app.post('/parsed',(req,res)=>{
+  console.log(access_token);
+  cloudvision(req,res,access_token);
+  res.render()
+});
 
 
 
--- Spotify_API_Connection/spotify.js
+++ Spotify_API_Connection/spotify.js
@@ -1,3 +1,4 @@
+
 var request = require("request");
 var SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi();
@@ -7,97 +8,111 @@ var clientSecret = '';
 var callbackURL = 'http://localhost:3000/logindone';
 
 
-module.exports = function(S){
-    // resp.header('Access-Control-Allow-Origin', '*');
-    // resp.header('Access-Control-Allow-Headers', 'X-Requested-With');
+module.exports = function(S, access_token){
+//     // resp.header('Access-Control-Allow-Origin', '*');
+//     // resp.header('Access-Control-Allow-Headers', 'X-Requested-With');
     
-    // client credential
+//     // client credential
 
-    var authOptions = {
-        method: 'POST',
-        url: 'https://accounts.spotify.com/api/token',
-        headers: 
-        { 
-            // 'Postman-Token': '',
-            // 'Cache-Control': 'no-cache',
-            'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64'))
-            // 'Content-Type': 'application/x-www-form-urlencoded' 
-        },
-        form: { grant_type: 'client_credentials' },
-        json: true
-    }
+//     var authOptions = {
+//         method: 'POST',
+//         url: 'https://accounts.spotify.com/api/token',
+//         headers: 
+//         { 
+//             // 'Postman-Token': '',
+//             // 'Cache-Control': 'no-cache',
+//             'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64'))
+//             // 'Content-Type': 'application/x-www-form-urlencoded' 
+//         },
+//         form: { grant_type: 'client_credentials' },
+//         json: true
+//     }
+
+//     return new Promise(function(resolve, reject) {
+//         // Do async job
+
+//         request(authOptions, function (error, response, body) {
+//             var token = body.access_token;
+
+//             var options = {
+//                 url: 'https://api.spotify.com/v1/recommendations',
+//                 qs: { seed_genres: S},
+//                 headers: {
+//                   'Authorization': 'Bearer '+ token
+//                 },
+//                 json: true
+//             };
+
+            
+//             request.get(options, function(error, response, body) {
+//                 console.log('in spotify',access_token)
+//                 // console.log(body);
+//                 var arrayofobject = body.tracks;
+//                 arrayofobject.map(function(item) {
+//                     console.log(item.external_urls.spotify);                           
+//                 })
+//             });
+
+//         });
+//     })
+
+    console.log('access token is', access_token);
 
     return new Promise(function(resolve, reject) {
-        // Do async job
 
-        request(authOptions, function (error, response, body) {
-            var token = body.access_token;
+        spotifyApi.setAccessToken(access_token);
 
-            var options = {
-                url: 'https://api.spotify.com/v1/recommendations',
-                qs: { seed_genres: S},
-                headers: {
-                  'Authorization': 'Bearer '+ token
-                },
-                json: true
-            };
+        var user_id;
 
-            
-            request.get(options, function(error, response, body) {
-                // console.log(body);
-                var arrayofobject = body.tracks;
-                arrayofobject.map(function(item) {
-                    console.log(item.external_urls.spotify);                           
-                })
-            });
-
+        spotifyApi.getMe()
+        .then(function(data) {
+            user_id = data.body.id
+            console.log('Some information about the authenticated user', data.body);
+        }, function(err) {
+            console.log('Something went wrong!', err);
         });
-    })
 
 
+        // var optionsCreatePlaylist = {
+        //     url: 'https://api.spotify.com/v1/users/'+user_id+'/playlists',
+        //     headers: {
+        //         'Authorization': 'Bearer '+ access_token,
+        //         'Content-Type': 'application/json'
+        //     },
+        //     json: true,
+        //     body: JSON.stringify({
+        //         'name': 'cool playlist',
+        //         'public':false
+        //     })
+        // };
 
-    // authorization code
+        // request.post(optionsCreatePlaylist, function(error, response, body){
+        //     console.log(body);
+        // });
 
-    // var authOptions = {
-    //     url: 'https://accounts.spotify.com/api/token',
-    //     form: {
-    //       code: c,
-    //       redirect_uri: callbackURL,
-    //       grant_type: 'authorization_code'
-    //     },
-    //     headers: {
-    //       'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64'))
-    //     },
-    //     json: true
-    // };
 
-    // return new Promise(function(resolve, reject) {
-    //     // Do async job
-
-    //     request.post(authOptions, function(error, response, body) {
-    //         console.log('body is',body);
-    //         var token = body.access_token;
-    //         spotifyApi.setAccessToken(token);
-
-    //         var options = {
-    //             url: 'https://api.spotify.com/v1/recommendations',
-    //             qs: { seed_genres: S},
-    //             headers: {
-    //               'Authorization': 'Bearer '+ token
-    //             },
-    //             json: true
-    //         };
-
-    //         request.get(options, function(error, response, body) {
-    //             console.log(body);
-    //             // var arrayofobject = body.tracks;
-    //             // arrayofobject.map(function(item) {
-    //             // console.log(item.external_urls.spotify);                           
-    //             // })
-    //         });
-    //     });
+        // get recommendation
+        var optionsRecommendation = {
+            url: 'https://api.spotify.com/v1/recommendations',
+            qs: { seed_genres: S},
+            headers: {
+                'Authorization': 'Bearer '+ access_token
+            },
+            json: true
+        };
             
-    // });
+        request.get(optionsRecommendation, function(error, response, body) {
+            var arrayofobject = body.tracks;
+                arrayofobject.map(function(item) {
+                    console.log(item.external_urls.spotify); 
+                    var track_id = item.id;
+                    console.log(track_id);
+                    spotifyApi.addToMySavedTracks([track_id]);                          
+            })
+        });
+
+
+    });
 
 }
 
